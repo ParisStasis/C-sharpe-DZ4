af43057f045a734294f707af041cd680bb9e53fe
Stas <caplan_74@mail.ru>
2023-02-28 00:37:23 +0500

    exp25

---
+++ exp 25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("ВВедите число А ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите число B ");
+int B = Convert.ToInt32(Console.ReadLine());
+int x =A;
+for (int i = 1; i < B; i++)
+{
+    x = x*A;
+}
+Console.WriteLine(" число А в натуральной степени числа В равно: " + x);
